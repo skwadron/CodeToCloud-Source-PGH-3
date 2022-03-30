a2142ee3459344d6145d9bb3f84f1c104eb460f3
Josh Almond <JoshAlmond@FabrikamMedical.example.org>
2022-03-30 16:39:35 +0000

    :rocket: Added instructions to build and run the application incl. AI.

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("[YOUR APPINSIGHTS KEY]");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
